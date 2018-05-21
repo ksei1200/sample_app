a28610032ad892bc4c70d43ea432e6e0fd169a51
EC2 Default User <ksei@jcom.home.ne.jp>
2018-05-21 10:37:42 +0000

    Finish CH10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy]
+
+  def index
+    @users = User.paginate(page:params[:page])   
+  end
 
   def show
     @user = User.find(params[:id])
@@ -23,6 +30,32 @@ class UsersController < ApplicationController
       render 'new' #newテンプレートを呼び出し
     end
   end
+  
+  # GET /users/:id/edit
+  # params[:id] => :id
+  def edit
+    # @user = User.find(params[:id])
+    # app/views/users/edit.html.erb
+  end
+  
+  # PATCH /users/:id
+  def update
+    # @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合を扱う。
+      flash[:success] = "プロファイルがアップデートされました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  # DELETE /users/:id
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "ユーザーは削除されました"
+    redirect_to users_url
+  end
 
 
   private
@@ -31,4 +64,26 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)  #paramsに:userのハッシュに戻す値を制限
     end
+    
+    # beforeアクション
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      if not logged_in?
+        store_location
+        flash[:danger] = "ログインしてください！！"
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+    
 end
