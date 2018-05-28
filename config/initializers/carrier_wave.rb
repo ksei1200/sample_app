e18bb9b0e1fcc1cc67afbe92f79a0afaa34197d7
EC2 Default User <ksei@jcom.home.ne.jp>
2018-05-28 12:18:26 +0000

    Finish CH13 withoutS3

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,12 +1,12 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
+#if Rails.env.production?
+#  CarrierWave.configure do |config|
+#    config.fog_credentials = {
       # Amazon S3用の設定
-      :provider              => 'AWS',
-      :region                => ENV['us-east-2a'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAIKN6U5NFSS2RVAOA'],
-      :aws_secret_access_key => ENV['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']
-    }
-    config.fog_directory     =  ENV['kseirails']
-  end
-end
+#      :provider              => 'AWS',
+#      :region                => ENV['us-east-2a'],     # 例: 'ap-northeast-1'
+#      :aws_access_key_id     => ENV['AKIAIKN6U5NFSS2RVAOA'],
+#      :aws_secret_access_key => ENV['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']
+#    }
+#    config.fog_directory     =  ENV['kseirai']
+#  end
+#end
