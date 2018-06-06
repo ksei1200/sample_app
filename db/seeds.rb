0b01a6974bfb82fd0a12574ef49dcdf1b5179b04
EC2 Default User <ksei@jcom.home.ne.jp>
2018-06-06 07:13:58 +0000

    Finish CH14

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
+#ユーザー
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar12",
@@ -17,8 +18,17 @@ User.create!(name:  "Example User",
                password_confirmation: password)
 end
 
+#マイクロポスト
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }               
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
